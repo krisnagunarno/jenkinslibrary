07e45abbd2ee99c760aa4aaed5c85d2ac06c1c5b
Matius Krisna Gunarno <54306640+krisnagunarno@users.noreply.github.com>
2021-02-08 19:02:15 +0700

    Create dockerutilities.groovy

---
+++ vars/dockerutilities.groovy
@@ -0,0 +1,40 @@
+#!/usr/bin/env groovy
+
+
+def call(Map param){
+        pipeline {
+
+          agent {
+            label 'manager'
+          }
+
+          stages {
+            stage ('Test Pipeline') {
+              steps {
+                echo "Show Docker 1"
+                sh '''
+                  docker --version
+                  docker image ls
+                  echo "You can do docker-compose, and other command here!!"
+                '''
+                }
+              }
+               stage ('Docker Stack Deploy') {
+                steps {
+                  echo "Deploying Services"
+                  script{
+                    def files = findFiles(glob: '*.yml')
+                    def command = "docker stack deploy "
+                    files.each {
+                      echo "${it.name}"
+                      command += "-c ${it.name} "
+                    }
+                    command += "future"
+                    echo "${command}"
+                    sh (script: "${command}", returnStdout: true)
+                }
+              }
+            }
+          }
+        }
+}
